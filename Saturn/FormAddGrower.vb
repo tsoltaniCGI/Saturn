4622e3ccd91fbebb88536fe121939600ac1fe774
tsoltaniCGI <tsoltani@columbiagrain.com>
2021-12-06 09:33:43 -0800

    New Layout

--- Saturn/FormAddGrower.vb
+++ Saturn/FormAddGrower.vb
@@ -215,4 +215,8 @@ Public Class FormAddGrower
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Close()
     End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
 End Class
