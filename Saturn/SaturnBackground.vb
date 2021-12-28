6feced3f081ce5d26fbea9472cd2fa7ae2fa135d
jschwartz8587 <JSchwartz@columbiagrain.com>
2021-12-28 10:47:19 -0800

    Changed back to Test

--- Saturn/SaturnBackground.vb
+++ Saturn/SaturnBackground.vb
@@ -3,7 +3,7 @@
     Private Sub SaturnBackground_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim formLogin As New FormLogin
         GlobalVariables.oFrmBackground = Me
-        GlobalVariables.sEnv = "P"
+        GlobalVariables.sEnv = "T"
         formLogin.TopLevel = True
         'formLogin.Parent = Me
         formLogin.ShowDialog()
