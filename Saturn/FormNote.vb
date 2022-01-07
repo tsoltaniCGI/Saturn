0389b8f3a463e71241b9acc9969866015af8783d
jschwartz8587 <JSchwartz@columbiagrain.com>
2022-01-07 09:24:10 -0800

    Fixed Bug with Edit button being disabled

--- Saturn/FormNote.vb
+++ Saturn/FormNote.vb
@@ -108,4 +108,8 @@ Public Class FormNote
     Private Sub lblGrowerCity_Click(sender As Object, e As EventArgs) Handles lblGrowerCity.Click
 
     End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
 End Class
