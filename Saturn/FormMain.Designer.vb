80685b8ad6771a4b914be1d81682e95fd11712ca
tsoltaniCGI <tsoltani@columbiagrain.com>
2022-01-27 09:46:54 -0800

    Update FormMain.Designer.vb

--- Saturn/FormMain.Designer.vb
+++ Saturn/FormMain.Designer.vb
@@ -785,9 +785,6 @@ Partial Class FormMain
         Me.AcceptButton = Me.btnSearch
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.AutoScroll = True
-        Me.AutoSize = True
-        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(126, Byte), Integer), CType(CType(131, Byte), Integer), CType(CType(150, Byte), Integer))
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
